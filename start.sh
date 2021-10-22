6f36669a8983cc942e8bb24a430e394427178403
luis.sobrinho <luis.consultorlinux@gmail.com>
2021-10-22 20:10:30 -0300

    Adicionando script de inicialização personalizado

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -n "${DEBUG}" ] && set -x
+
 IP="$(hostname -I | awk '{print $1}')"
 
 echo -e "SEU IP E: $IP"
@@ -8,13 +10,43 @@ sed "s/ALTERAR_IP/$IP/g" .env.template > .env
 
 source ./.env
 
+echo -e "Baixando as imagens necessárias"
+
+docker-compose pull
+
 echo -e "O endereço que será configurado no seu arquivo de hosts será: ${IP} ${GITLAB_URL}"
+
+docker-compose up etchosts
+
+grep ${GITLAB_URL} /etc/hosts
 
 echo -e "Iniciando o GIT"
 
-docker-compose up -d
+docker-compose up -d > /dev/null
 
-./wait-for-it.sh ${GITLAB_URL}/-/readiness -- echo "GIT ONLINE"
+echo -e "Esperando a URL ficar online"
+echo -e "Este processo pode levar vários minutos...."
+
+while [ true ]
+do
+    curl ${GITLAB_URL}/-/readiness --connect-timeout 15 &> /dev/null
+    if [[ "$?" -eq 0 ]]; then
+            curl ${GITLAB_URL}/users/sign_in --connect-timeout 15 &> /dev/null
+            if [[ "$?" -eq 0 ]]; then
+                echo "SUCESSO"
+                break
+            else
+                # echo -e "Erro, será realizado nova tentativa"
+                sleep 15
+            fi
+    else
+        # echo -e "Erro, será realizado nova tentativa"
+        sleep 15
+    fi
+done
+
+echo -e "Aguarde...."
+sleep 30
 
 echo -e "Registrando os Runners"
 
@@ -32,6 +64,6 @@ docker-compose exec runner gitlab-runner register \
 
 echo -e "Tudo pronto, abrindo o git"
 
-sleep 10
+sleep 3
 
 firefox ${GITLAB_URL}
